2cce88ddfb6985e814fe66bb4b53dcdc4bc75f44
Wes Vaske <wes.vaske@gmail.com>
2019-05-27 16:26:44 -0500

    Refactored to be installable package

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pbk',
+    version='0.1',
+    packages=['pbk', 'pbk.util', 'pbk.benchmarks'],
+    url='https://github.com/wvaske/pbk',
+    license='MIT License',
+    author='Wes Vaske',
+    author_email='wes.vaske@gmail.com',
+    description="Perflosopher's Benchmarking Kit is a set of scripts to automate system benchmarks and data collection"
+)
